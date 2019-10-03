6fac244442dde329b3b749980713a511eb07efbd
jnavarrete@k2ins.com <jnavarrete@k2ins.com>
2019-10-03 14:32:49 -0700

    adding third choice

--- tutorial_rest_project/basic/models.py
+++ tutorial_rest_project/basic/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class First(models.Model):
-    form_choices = [('A', 'A'), ('B', 'B')]
+    form_choices = [('A', 'A'), ('B', 'B'),('C', 'C')]
     name = models.CharField(null=True, blank=True, max_length=100)
     form_type = models.CharField(null=True, blank=True, max_length=100, choices=form_choices)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -13,7 +13,7 @@ class First(models.Model):
 
 class Second(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    form_choices = [('A', 'A'), ('B', 'B')]
+    form_choices = [('A', 'A'), ('B', 'B'), ('C', 'C'),]
     name = models.CharField(null=True, blank=True, max_length=100)
     form_type = models.CharField(null=True, blank=True, max_length=100, choices=form_choices)
     date_created = models.DateTimeField(auto_now_add=True)
